1a410c0f09b428243ea08723460223f073847e6b
root <root@localhost.localdomain>
2026-09-21 12:32:10 +0000

    fix(android): keep Zeptun NDK inputs inside project

--- android-app/app/src/main/cpp/Android.mk
+++ android-app/app/src/main/cpp/Android.mk
@@ -2,14 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := zeptun-prebuilt
-LOCAL_SRC_FILES := ../../../../third_party/zeptun/zig-out/android/prebuilt/$(TARGET_ARCH_ABI)/libzeptun.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../../third_party/zeptun/include
+LOCAL_SRC_FILES := zeptun-libs/$(TARGET_ARCH_ABI)/libzeptun.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/zeptun-include
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := zeptun-jni
 LOCAL_SRC_FILES := zeptun_jni.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../third_party/zeptun/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/zeptun-include
 LOCAL_STATIC_LIBRARIES := zeptun-prebuilt
 LOCAL_LDLIBS := -llog
 include $(BUILD_SHARED_LIBRARY)
